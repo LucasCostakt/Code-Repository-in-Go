17733e24cf8a8c3abc5198e767d7a05ee1dca912
Lucas <lucascosta.kt@gmail.com>
2020-03-03 16:10:03 -0300

    Create rot13.go

---
+++ rot13.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+fun rot13(b byte) byte{
+	var a, z byte
+	switch {
+		case 'a' <= b && b <= 'z':
+			a, z = 'a', 'z'
+		case 'A' <= b && b <= 'Z':
+			a, z = 'A', 'Z'
+		default:
+			return b
+		}
+	return (b-a+13)%(z-a+1)+a
+}
+
+func (rot rot13Reader) Read(p []byte) (n int, err error) {
+	
+	 n, err = rot.r.Read(p)
+	 x = "Lbh penpxrq gur pbqr!"
+	
+	for i := 0;  i < n; i++{
+		p[i]=rot13(p[i])
+	}
+	return 
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
